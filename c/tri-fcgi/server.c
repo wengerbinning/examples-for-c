95c269800f582637b18d2f0a0aca4a626f92b231
Wenger Binning <wengerbinning@163.com>
2023-06-08 19:42:10 +0800

    c(tri fastcgi): update

--- c/tri-fcgi/server.c
+++ c/tri-fcgi/server.c
@@ -7,6 +7,7 @@ static FCGX_Request fcgx_req;
 
 int main (int argc, char *argv[]) {
     char *buf;
+    char *cli_addr, *cli_port;
     static FCGX_Request *request = &fcgx_req;
     int sock;
 
@@ -20,12 +21,11 @@ int main (int argc, char *argv[]) {
     FCGX_InitRequest(request, sock, 0);
 
     while (1) {
-        printf("server running ...\n");
-
         FCGX_Accept_r(request);
 
-        buf = FCGX_GetParam("REMOTE_ADDR", request->envp);
-        FCGX_FPrintF(request->out, "remote addr: \"%s\"\n", buf);
+        cli_addr = FCGX_GetParam("REMOTE_ADDR", request->envp);
+        cli_port = FCGX_GetParam("REMOTE_PORT", request->envp);
+        printf("Accept %s:%s", cli_addr, cli_port);
 
         // buf = FCGX_GetParam("REQUEST_METHOD", request->envp);
         // printf("request method: \"%s\"\n", buf);
