dacc98ccf54ff2d623ac858bb14b29ab25c0e458
Wenger Binning <wengerbinnning@163.com>
2023-06-18 15:52:26 +0800

    c: update compiler

---
+++ c/compiler/preprocessing/preprocessing.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+
+
+int main(int argc, char *argv[]) {
+	
+
+
+if defined(DEBUG)
+	printf("[%s(%d) debug] run in here", __func__, __LINE__);
+#endif
+
+	
+	return 0;
+}	
+
