2b06e1f803031c8f6717802c2b6e6e92de4a02d1
wbzheng <wbzheng@grandstream.cn>
2023-08-30 10:36:32 +0800

    shell: update shell tool framework

--- shell/libexec/shell.sh
+++ shell/libexec/shell.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-
 PROG=${0##*/}
 DATE="2023-08-29"
 VERSION="v0.0.0.1"
 CMD="help"
 CMD_LOCK=false
 VERBOSE=false
-
-
+unset LABEL SERVER
 #-----------------------------------------------------------------------------
-
 HELP=$(cat <<EOF
  $PROG is a shell script tool framework.
  
@@ -21,8 +18,21 @@ HELP=$(cat <<EOF
 	-h, --help
 	-V, --version
 
+ EXTISTIONS:
+ 	--push
+	--pull
+	--archive
+
  OPTIONS:
+ 	-l, --label
 	-v, --verbose
+	-s, --server
+
+ EXAMPLES:
+	-0- $PROG --help
+	-0- $PROG --version
+	-0- $PROG --verbose --help
+	-1- $PORG --verbose --version
 
 EOF
 )
@@ -30,16 +40,19 @@ EOF
 
 ##############################################################################
 
+#-----------------------------------------------------------------------------
 # usage: help [<prog>]
 help() {
 	echo -e "$HELP"
 }
 
+#-----------------------------------------------------------------------------
 # usage: version [<prog>]
 version() {
 	echo "Version:$VERSION($DATE)"
 }
 
+#-----------------------------------------------------------------------------
 # usage: main 
 main() {
 	case $CMD in
@@ -54,11 +67,18 @@ main() {
 }
 ##############################################################################
 #
-while test -n "$1"; do
-    case $1 in
+idx=0
+while test -n "$1"; do cmd=$1; shift; idx=$((idx + 1))
+	[ "$DEBUG" = true ] && {
+		echo "$idx $cmd"
+	}
+    case $cmd in
     -h|--help)
         CMD="help"
 		CMD_LOCK=true
+	;;
+	-l|--label)
+		LABEL=$1; shift
     ;;
     -V|--version)
         test "$CMD_LOCK" == true || CMD="version"
@@ -68,8 +88,12 @@ while test -n "$1"; do
         VERBOSE=true
     ;;
     esac
-shift; done
+done
 
-[ "$VERBOSE" == true ] && echo "CMD:$CMD"
+[ "$VERBOSE" == true ] && {
+	echo "CURRENT COMMAND: $CMD"
+	echo "VERBOSE ATTR: $VERBOSE"
+	echo "LABEL ATTR: $LABEL"
+}
 #
 main "$@"
