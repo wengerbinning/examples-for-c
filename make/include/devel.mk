4562ea26351b830926c44ab67f95c02b58cba8ad
wbzheng <wbzheng@grandstream.cn>
2024-01-26 21:07:35 +0800

    2024-01-26 0000

--- make/include/devel.mk
+++ make/include/devel.mk
@@ -1,5 +1,5 @@
 
-define devel_error
+define devel/rule/error
 	@echo -ne '\e[31m'
 	@echo -ne "develop - "
 	@echo -ne $1
@@ -7,7 +7,7 @@ define devel_error
 	@echo -ne '\n'
 endef
 
-define devel_warning
+define devel/rule/warning
 	@echo -ne '\e[33m'
 	@echo -ne "develop - "
 	@echo -ne $1
@@ -15,7 +15,7 @@ define devel_warning
 	@echo -ne '\n'
 endef
 
-define devel_notice
+define devel/rule/notice
 	@echo -ne '\e[34m'
 	@echo -ne "develop - "
 	@echo -ne $1
@@ -23,10 +23,18 @@ define devel_notice
 	@echo -ne '\n'
 endef
 
-define devel_info
+define devel/rule/info
 	@echo -ne '\e[32m'
 	@echo -ne "develop - "
 	@echo -ne $1
 	@echo -ne '\e[0m'
 	@echo -ne '\n'
+endef
+
+define devel/make/notice
+	$(info $1)
+endef
+
+define devel/make/info
+	$(info $1)
 endef
