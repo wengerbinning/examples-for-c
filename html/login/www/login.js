b0da9c69b14e952085e3c9dc3870a4f03f41ded3
Wenger Binning <wengerbinning@163.com>
2024-11-18 07:13:04 +0800

    Add html/text

---
+++ html/login/www/login.js
@@ -0,0 +1,34 @@
+
+let loginForm = document.forms["login"];
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const url = "/api/login.pl";
+
+    let form = e.srcElement;
+    let username = form.elements.namedItem("username");
+    let password = form.elements.namedItem("password");
+    const data = {
+        username: username.value,
+        password: password.value,
+    };
+
+    console.log(username.value);
+    console.log(password.value);
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: JSON.stringify(data),
+        
+        // headers: headers,
+        // crossDomain: true, // FIXME - CORS
+        // beforeSend: function(request) {
+        //     request.setRequestHeader("Access-Control-Allow-Method", "GET, POST, OPTIONS");
+        //     request.setRequestHeader("Access-Control-Allow-Origin", "*");
+        // },
+      }).done(function (data) {
+        console.log(data);
+      });
+});
