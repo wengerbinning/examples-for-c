392c10930d8c2270c8479d7e142d345712d76fc7
Wenbin Zheng <wbzheng@grandstream.cn>
2024-12-03 14:28:05 +0800

    test ip address pool

--- c/std-libc/network/main.c
+++ c/std-libc/network/main.c
@@ -25,19 +25,41 @@ submask: 255.255.255.0
  */
 
 
-struct ip_address {
-    int addr, mask;
+// 256
+
+// 8 8 8 8
+
+
+struct ipmap {
+    struct ipmap *prev, *next;
+    int idx, map;
 };
 
+struct subnet {
+    int addr, mask;
+    int idx1, idx2, idx3, idx4;
+    struct ipmap cur;
 
+    struct ipmap *static_pool, *active_pool;
+    // struct ipmap *reserv_pool, *forbid_pool;
+};
 
 /* ipaddr_t */
-
+#define IP2STR_ZERO
 #define IPFMT0 "%hhu.%hhu.%hhu.%hhu"
-#define _IPOFST(addr, n) *((unsigned char *)addr + n)
+#define _IPOFST(addr, n) *((const unsigned char *)addr + n)
 #define IP2STR(a) _IPOFST(a, 3),_IPOFST(a, 2),_IPOFST(a, 1),_IPOFST(a, 0)
 
+#define SUBNETFMT0 "%hhu.%hhu.%hhu.%hhu/%hhu"
 
+#define SUBNET2STR(subnet)
+
+#define BIT(n)          1UL << n
+#define MASK(m, n)
+
+int submask(int mask) {
+    return 1 << (32 - mask) -1;
+}
 
 int main (int argc, char *argv) {
     // regex_t regex;
@@ -55,17 +77,40 @@ int main (int argc, char *argv) {
     // }
     // regfree(&regex);
 
-
-    struct ip_address ip;
+    struct subnet subnet;
 
     // char addr[] = {0, 1, 4, 2};
     int addr = 25;
+    int idx, map;
+    subnet.addr = 0x01010101;
+    subnet.mask = 24;
+
+    addr = submask(24);
+
+    subnet.idx4 = 192;
+    subnet.idx3 = 168;
+    subnet.idx2 = 0;
+    subnet.idx1 = 0;
+    subnet.map = 0xFFFFFFFF;
+
+    idx = (subnet.idx1) + (subnet.idx2 * 8) + (subnet.idx3 * 256 * 8) +
+        (subnet.idx4 * 256 * 256 * 8);
+    map = subnet.map;
+
+    for (int i = 0; i < 32; i++) {
+        if ((1 << i) & map) {
+            addr = (idx * 32) + i;
+            printf ("%08x " IPFMT0 " %2u\n", addr, IP2STR(&addr), addr);
+        }
+    }
 
 
 
-    printf (IPFMT0"\n", IP2STR(&addr));
+    addr = subnet.idx * 8;
+
+    // printf (IPFMT0"\n", IP2STR(&addr));
+
 
 
     return 0;
 }
-
