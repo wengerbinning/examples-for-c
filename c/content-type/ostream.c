15b1a044332fb2cbdd45fa172456600d543019ee
Wenbin Zheng <wbzheng@grandstream.cn>
2024-12-28 20:01:08 +0800

    2024-12-28 01

--- c/content-type/ostream.c
+++ c/content-type/ostream.c
@@ -10,7 +10,7 @@
 #include "iostream.h"
 
 
-#define BUFIZE 32
+#define BUFIZE 16
 
 
 
@@ -20,7 +20,7 @@
 #define log_info(fmt, ...)      printf("\033[37m" fmt "\033[0m\n", ## __VA_ARGS__ )
 #define log_debug(fmt, ...)     printf("\033[34m" fmt "\033[0m\n", ## __VA_ARGS__ )
 
-
+// 4 + 4 + 2
 
 // ============================================================================
 static int
@@ -30,78 +30,75 @@ fio_input_multipart_form_data(
 )
 // ============================================================================
 {
+    size_t dofst = 0, rofst = 0;
     char *p0, *p1;
-    size_t ofst, size, len;
-    size_t cofst, dofst, rofst, plen;
+    size_t ofst, size, len, cofst, plen;
     char *boundary;
     char buffer[BUFIZE + 1];
     static bool start;
     static int idx = 0;
     static bool cache = false;
-    static size_t clen = 0      ;
+    static size_t clen = 0;
     static char cbuffer[BUFIZE + 1];
-
-
     boundary = ctx->content_boundary;
+
 
     log_warning("read %d(0x%x) data ...", dlen, dlen);
     hexdump(data, dlen);
 
     /* check cache */
 
-    // 如果存在cache, 则记录clen的位置;
-    // 复制一部分数据到cache
-    // 记录复制的数据偏移dofst
+    // 1. 如果存在cache, 则记录clen的位置;
+    // 2. 将cache填充满
+    // 3. 记录偏移rofst
+    // 4. 更新指针
     p0 = data;
     plen = dlen;
-    dofst = 0;
-    rofst = 0;
-    if ( cache ) {
+    if (cache) {
         cofst = clen;
-
-        log_debug("found cache");
-        hexdump(cbuffer, clen);
-
         len = (dlen < (BUFIZE - clen)) ? dlen : (BUFIZE - clen);
         memcpy(cbuffer + cofst, data, len);
         clen += len;
+        rofst = len;
+
+        log_debug("cache0 data (cache:%d/%d, data:%d/%d)", cofst, clen, dofst, rofst);
+        hexdump(cbuffer, clen);
+
         p0 = cbuffer;
         plen = clen;
-        rofst = len;
     }
-
-    log_debug("dofst: 0x%x,  rofst: 0x%x", dofst, rofst);
 
     /* check boundary */
     check_boundary:
-    if (cache) {
-        log_debug("check in cache: %d/%d %p/%d %p/%d %s", cofst, clen, data, dofst, p0, plen, boundary);
-        hexdump(p0, plen);
-    } else {
-        log_debug("check in data: %p/%d %p/%d %s", data, dofst, p0, plen, boundary);
-    }
+    // if (cache) {
+    //     log_debug("check in cache (cache:%d/%d, data:%d/%d, handle:%d)",
+    //         cofst, clen, dofst, rofst, plen);
+    // } else {
+    //     log_debug("check in data (data:%d/%d, handle:%d)",dofst, rofst, plen);
+    // }
+    // hexdump(p0, plen);
+
     while ( (p1 = (char *)memmem(p0, plen, boundary, strlen(boundary))) ) {
         bool suffix = false;
+
         size  = p1 - p0;
         plen -= size;
-
         if ((4 <= size) && (0 == strncmp(p1 - 4, "\r\n--", 4))) {
             memset(p1 - 4, 0, 2);
             size -= 4;
         } else if ( (2 <= size) && 0 == strncmp(p1 - 2, "--", 2) ) {
             /* first data */
-            log_notice("==== START ====");
             size -= 2;
             idx = 1;
+            log_notice("==== START ====");
         }
 
         /* write form data end */
         if ( 0 < size ) {
-            log_notice("index: %d, start: %5s, cache: %5s => %s", idx++,
-                start ? "true" : "false",
-                cache ? "true" : "false",
-                p0);
-            // hexdump(p0, size);
+            log_notice("index: %d, start: %5s, cache: %5s => %d:%s", idx++,
+                start ? "true" : "false", cache ? "true" : "false",
+                strlen(p0), p0);
+            hexdump(p0, size);
         }
 
         /* next form */
@@ -109,78 +106,82 @@ fio_input_multipart_form_data(
         p0 = p1 + strlen(boundary);
         plen -= strlen(boundary);
 
+
+        /* update pointer */
+        if (cache && (p0 < (cbuffer + cofst))) {
+            ofst = (cbuffer + cofst) - p0;
+            len = (cbuffer + clen) - p0;
+            memcpy(buffer, p0, len);
+            memcpy(cbuffer, buffer, len);
+            cache = true;
+            cofst = ofst;
+            clen = len;
+            /* copy data */
+            ofst = clen;
+            size = BUFIZE - clen;
+            len = ((dlen - rofst) < size) ? (dlen - rofst) : size;
+            memcpy(cbuffer + ofst, (data + rofst), len);
+            rofst += len;
+            clen += len;
+
+                log_debug("update1 cache (%d/%d, %d/%d)", cofst, clen, dofst, rofst);
+                hexdump(cbuffer, clen);
+
+            /* update pointer */
+            p0 = cbuffer;
+            plen = clen;
+        /* */
+        } else if (cache) {
+            // ofst = p0 - (cbuffer + cofst);
+            // log_debug("change1Q cache to data 0x%x/0x%x %d, len:%d", dofst, rofst, ofst, plen);
+            // hexdump(p0, plen);
+
+            dofst += p0 - (cbuffer + cofst);
+            p0 = data + dofst;
+            plen = dlen - dofst;
+
+            // if (2 <= plen && 0 == strncmp(p0, "\r\n", 2)) {
+            //     /* skip boundary suffix: \r\n */
+            //     p0 += 2;
+            //     plen -= 2;
+            //     dofst += 2;
+            //     suffix = true;
+            // }
+
+            cache = false;
+            rofst = dofst;
+
+            log_debug("change1 cache to data 0x%x/0x%x len:%d", dofst, rofst, plen);
+            hexdump(p0, plen);
+        }
+
+
+        /* */
         if (plen == 4 && 0 == strncmp(p0, "--\r\n", 4)) {
-            log_notice("==== END ====");
+            log_notice("==== END1 ====");
             return 0;
-        } else if (2 < plen && 0 == strncmp(p0, "\r\n", 2)) {
+        } else if (2 <= plen && 0 == strncmp(p0, "\r\n", 2)) {
             /* skip boundary suffix: \r\n */
             p0 += 2;
             plen -= 2;
             suffix = true;
         }
 
-        // printf("@@@@@ start: %s, cache: %s\n",
-        //     start ? "true" : "false",
-        //     cache ? "true" : "false");
-        // hexdump(p0, plen);
-
-
-        // if (cache) {
-        //     printf("%d/%d/%d  %d\n", p0 - cbuffer, cofst, clen, dofst);
-        // }
-
-        /* update pointer */
-        if (cache && (p0 < (cbuffer + cofst))) {
-            cofst = (cbuffer + cofst) - p0;
-            len = (cbuffer + clen) - p0;
-            memcpy(buffer, p0, len);
-            memcpy(cbuffer, buffer, len);
-            cache = true;
-            clen = len;
-
-            size = BUFIZE - clen;
-            /* copy data */
-            len = ((dlen - dofst) < size) ? ((dlen - dofst)) : size;
-            memcpy(cbuffer + clen, (data + dofst), len);
-            clen += len;
-            /* update pointer */
-            p0 = cbuffer;
-            plen = clen;
-        } else if (cache) {
-
-            ofst = p0 - (cbuffer + cofst);
-            dofst += ofst;
-            p0 = data + (dofst);
-            plen = dlen - (dofst);
-            cache = false;
-
-            if (2 < plen && 0 == strncmp(p0, "\r\n", 2)) {
-                /* skip boundary suffix: \r\n */
-                p0 += 2;
-                plen -= 2;
-                suffix = true;
-            }
-            log_debug("change cache to data 0x%x/0x%x", dofst, ofst);
-        }
-
         /* */
-        if ( !cache && plen <= 2 ) {
+        if (!cache && !suffix) {
             len = plen + strlen(boundary);
-            len += suffix ? 2 : 0; // FIXME - suffix: /r/n
-            log_debug("cache1 data %d/%d/%d", len, plen, strlen(boundary));
-            hexdump(p1, len);
-            memcpy(cbuffer, p1, len);
+            memcpy(cbuffer, (p0 - strlen(boundary)), len);
             cache = true;
             clen = len;
-            return 0;
-        } else if (!cache) {
+
+
+        }
+        if (!cache) {
             dofst = p0 - data;
             rofst = dofst;
         }
-
-        // printf("offset:0x%04x, size:%d, len:%d\n", dofst, size, plen);
-
     }
+
 
     /* check end */
     p0 = cache ? cbuffer : (data + dofst);
@@ -188,79 +189,71 @@ fio_input_multipart_form_data(
     if ( plen == 4 && 0 == strncmp(p0, "--\r\n", 4) ) {
         log_notice("==== END ====");
         return 0;
-    } else if (0 < plen) {
-        // hexdump(p0, plen);
     }
 
+
     /* update cache */
-
     if ( cache && BUFIZE <= clen ) {
-        // printf("update cache 2\n");
-        // hexdump(cbuffer, clen);
-
-        // cache = false;
-
         len = clen;
         size = strlen(boundary) + 4 + 3;
-        if ( size <= BUFIZE ) {
-            len -= size;
-        }
-
+        len -= (size <= BUFIZE) ? size : 0;
         /* */
-        // if (start)
-        //     idx++;
-
+        memset(buffer, 0, sizeof(buffer));
         memcpy(buffer, cbuffer, len);
-        log_notice("#index: %d, start: %5s, cache: %5s => %s", idx,
-                start ? "true" : "false",
-                cache ? "true" : "false",
-                buffer);
-        // log_debug("demo");
+
+        log_notice("#index: %d, start: %5s, cache: %5s => %d:%s", idx,
+            start ? "true" : "false", cache ? "true" : "false",
+            strlen(buffer), buffer);
         hexdump( buffer, len );
-        // if ( form_data_write(&form, cbuffer, clen) != clen ) {
-        //     form_data_exit( &form );
-        //     ret = ctx->retVal;
-        //     // Log(LOG_DEBUG, "return code %d", ret);
-        //     goto exit0;
-        // }
-        start = false;
 
         /* */
+        start = false;
         cache = false;
-        size = strlen(boundary) + 4 + 3;
-        if ( size <= BUFIZE ) {
-            len = size;
-            ofst = clen - len;
-            memcpy(buffer, cbuffer + ofst, len);
-            memcpy(cbuffer, buffer, len);
-            cache = true;
-            clen = len;
+        if (cofst < len) {
+            dofst = len - cofst;
+            rofst = dofst;
+            p0 = data + dofst;
+            plen = dlen - dofst;
 
-            log_debug("cache2 data %d/%ld", cofst, clen);
-            hexdump(cbuffer + ofst, len);
+            log_debug("change2 cahce to data (data:%d/%d)", dofst, rofst);
+            hexdump(p0, plen);
 
-            p0 = cbuffer;
-            plen = clen;
-            // goto check_boundary;
+        } else {
+            size = strlen(boundary) + 4 + 3;
+            if ( size <= BUFIZE ) {
+                ofst = clen - size;
+                memcpy(buffer, cbuffer + ofst, size);
+                memcpy(cbuffer, buffer, size);
+                cache = true;
+                cofst -= len;
+                clen = size;
+
+                log_debug("update2 cache (cahce:%d/%d, data:%d/%d)", cofst, clen, dofst, rofst);
+                hexdump(cbuffer, clen);
+
+                p0 = cbuffer;
+                plen = clen;
+            }
         }
         cofst = clen;
     }
 
     /* cache data */
-    // printf("dofst: %d,  rofst: %d\n", dofst, rofst);
-
-    cofst = cache ? clen : 0;
-    size = cache ? BUFIZE - cofst : BUFIZE;
+    ofst = cache ? cofst : 0;
+    len = cache ? clen : 0;
+    clen = len;
+    size = cache ? (BUFIZE - clen) : BUFIZE;
     len = (size < (dlen - rofst)) ? size : (dlen - rofst);
     if (0 < len) {
-        memcpy((cbuffer + cofst), (data + rofst), len);
+        memcpy((cbuffer + ofst), (data + rofst), len);
         cache = true;
-        clen = cofst + len;
-
-        log_debug("cache3 data %d/%ld", cofst, clen);
-        hexdump(data + rofst, len);
+        cofst = ofst;
+        clen += len;
         dofst = rofst;
         rofst += len;
+
+        log_debug("cache2 data (cahce:%d/%d, data:%d/%d)", cofst, clen, dofst, rofst);
+        hexdump(cbuffer, clen);
 
         p0 = cbuffer;
         plen = clen;
